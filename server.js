cbb9062a559d6dbe36b5d6f772a32d15485ab9b1
LucasBrad <lucasbradpotage6@gmail.com>
2022-09-10 17:32:10 +0400

    tems

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req,res)=>{
-    res.render('index')
-})
+// app.get('/', (req,res)=>{
+//     res.render('index')
+// })
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('home')
 })
 
