8b8469db3d82c13c791d94cce1a548071501ad49
LucasBrad <lucasbradpotage6@gmail.com>
2022-09-14 19:32:59 +0400

    details added

--- server.js
+++ server.js
@@ -12,12 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-// app.get('/', (req,res)=>{
-//     res.render('index')
-// })
-
-app.get('/',(req,res)=>{
-    res.render('home')
+app.get('/', (req,res)=>{
+    res.render('index')
 })
 
 app.get('/about',(req,res)=>{
